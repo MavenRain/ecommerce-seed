88b953bf4604eb3010f2cb980b6d84edbff88325
Onyeka Obi <Onyeka.Obi@gmail.com>
2021-05-05 22:35:47 -0700

    Further abstract RepositoryLaws

--- server/src/test/scala/io/github/mavenrain/persistence/RepositoryLaws.scala
+++ server/src/test/scala/io/github/mavenrain/persistence/RepositoryLaws.scala
@@ -1,17 +1,20 @@
 package io.github.mavenrain.persistence
 
+import org.scalacheck.Arbitrary
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import scala.util.chaining.scalaUtilChainingOps
 import zio.{IO, Runtime}
+
 trait RepositoryLaws  {
   this: AnyFlatSpec with Matchers with RepositoryProvider with ScalaCheckDrivenPropertyChecks =>
-  type Error = String
-  type Item = (Int, String, Boolean)
-  type Id = String
-  type RepositoryCreator = Seq[Item] => Repository[String, Item]
+  type Item
+  type Id
+  type RepositoryCreator = Seq[Item] => Repository[Id, Item]
   val createRepository: RepositoryCreator
+  protected implicit val arbitraryItem: Arbitrary[Item]
+  protected implicit val arbitraryId: Arbitrary[Id]
   
   forAll { (item: Item, id: Id) =>
     Runtime.default.unsafeRun(
